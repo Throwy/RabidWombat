14fa585bf2c9370e887637b68298b109f86c02f0
Gavin <throwy94@gmail.com>
2019-03-12 16:17:57 -0400

    hooks all done

--- RabidWombat.Hooks/Windows/HookManager.cs
+++ RabidWombat.Hooks/Windows/HookManager.cs
@@ -38,11 +38,118 @@ namespace RabidWombat.Hooks.Windows
         /// </summary>
         private event GlobalKeyEventHandler GlobalKeyDown;
 
+        /// <summary>
+        /// Raised whenever the global mouse hook detects a mouse move action.
+        /// </summary>
+        public event GlobalMouseEventHandler MouseMove
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                GlobalMouseMove += value;
+            }
+            remove
+            {
+                GlobalMouseMove -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
 
+        /// <summary>
+        /// Raised whenever the global mouse hook detects a mouse down action.
+        /// </summary>
+        public event GlobalMouseEventHandler MouseDown
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                GlobalMouseDown += value;
+            }
+            remove
+            {
+                GlobalMouseDown -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
 
+        /// <summary>
+        /// Raised whenever the global mouse hook detects a mouse up action.
+        /// </summary>
+        public event GlobalMouseEventHandler MouseUp
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                GlobalMouseUp += value;
+            }
+            remove
+            {
+                GlobalMouseUp -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        /// <summary>
+        /// Raised whenever the global mouse hook detects a mouse wheel action.
+        /// </summary>
+        public event GlobalMouseEventHandler MouseWheel
+        {
+            add
+            {
+                EnsureSubscribedToGlobalMouseEvents();
+                GlobalMouseWheel += value;
+            }
+            remove
+            {
+                GlobalMouseWheel -= value;
+                TryUnsubscribeFromGlobalMouseEvents();
+            }
+        }
+
+        /// <summary>
+        /// Raised whenever the global keyboard hook detects a key up action.
+        /// </summary>
+        public event GlobalKeyEventHandler KeyUp
+        {
+            add
+            {
+                EnsureSubscribedToGlobalKeyboardEvents();
+                GlobalKeyUp += value;
+            }
+            remove
+            {
+                GlobalKeyUp -= value;
+                TryUnsubscribeFromGlobalKeyboardEvents();
+            }
+        }
+
+        /// <summary>
+        /// Raised whenever the global keyboard hook detects a key down action.
+        /// </summary>
+        public event GlobalKeyEventHandler KeyDown
+        {
+            add
+            {
+                EnsureSubscribedToGlobalKeyboardEvents();
+                GlobalKeyDown += value;
+            }
+            remove
+            {
+                GlobalKeyDown -= value;
+                TryUnsubscribeFromGlobalKeyboardEvents();
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribes from all hooks and releases all resources associated with this object.
+        /// </summary>
         public void Dispose()
         {
-            throw new NotImplementedException();
+
+            // Force unsubscription from all hooks.
+            ForceUnsunscribeFromGlobalKeyboardEvents();
+            ForceUnsunscribeFromGlobalMouseEvents();
+
         }
     }
 }
